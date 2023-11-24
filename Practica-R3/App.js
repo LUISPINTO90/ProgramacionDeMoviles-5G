5867431266daa9e13dcc2c9ca247c1c2e5ee66c6
Luis Pinto <lpinto1@ucol.mx>
2023-11-24 09:36:51 -0600

    Fix navigation in App.js

--- Practica-R3/App.js
+++ Practica-R3/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Actions, Scene, Router } from "react-native-router-flux";
 import LoginScreen from "./src/LoginScreen";
 import HomeView from "./src/HomeView";
+import ArtistDetailView from "./src/ArtistDetailView";
 
 const scenes = Actions.create(
   <Scene key="root">
-    <Scene key="login" component={LoginScreen} hideNavBar initial />
-    <Scene key="main" component={HomeView} hideNavBar />
+    <Scene key="login" component={LoginScreen} hideNavBar />
+    <Scene key="main" component={HomeView} back />
+    <Scene key="artistDetail" component={ArtistDetailView} back />
   </Scene>
 );
 
--- Practica-R3/src/HomeView.js
+++ Practica-R3/src/HomeView.js
@@ -3,7 +3,6 @@ import { StyleSheet, View } from "react-native";
 import { Router, Scene, Stack } from "react-native-router-flux";
 
 import ArtistList from "./ArtistList";
-import ArtistDetailView from "./ArtistDetailView";
 import { getMusicData } from "./api-client";
 
 export default class HomeView extends Component {
@@ -19,20 +18,9 @@ export default class HomeView extends Component {
     const artists = this.state.artists;
 
     return (
-      <Router>
-        <Stack key="root">
-          <Scene
-            key="artistList"
-            component={() => <ArtistList artists={artists} />}
-            title="Top Artists"
-          />
-          <Scene
-            key="artistDetail"
-            component={ArtistDetailView}
-            title="Artist Detail"
-          />
-        </Stack>
-      </Router>
+      <View style={styles.container}>
+        {artists && <ArtistList artists={artists} />}
+      </View>
     );
   }
 }
